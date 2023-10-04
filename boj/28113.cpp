dfdd415dafbc0be36ef34decafa3da184504f963
daehyun <daehyuh@hufs.ac.kr>
2023-10-04 15:29:11 +0900

    10월 4일

---
+++ boj/28113.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (b < c){
+        cout << "Bus";
+    } else if (b > c){
+        cout << "Subway";
+    } else {
+        cout << "Anything";
+    }
+}
