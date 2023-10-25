26c36dba736232f9f0b57b26709659a8e8eb56ec
대현 <rkdeown10@naver.com>
2023-10-25 19:21:10 +0900

    10월25일

---
+++ boj/2845.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    
+    cout << a-n*m << " " << b-n*m << " " << c-n*m << " " << d-n*m << " " << e-n*m << endl;
+
+    return 0;
+}
